7b1e0fb4b7da5de7473d00c7eb15e98c1e4e87b3
gaurrav1 <22amtics009@gmail.com>
2025-06-21 17:17:50 +0530

    Project Initialize

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig({
+  base: './',
+  build: {
+    assetsInlineLimit: 0,
+    outDir: 'target',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
+      },
+    },
+  },
+  plugins: [react()],
+})
