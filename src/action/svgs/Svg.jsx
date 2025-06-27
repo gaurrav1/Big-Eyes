399b2fc7b47e915aa2daf87c6de7f364175078eb
gaurrav1 <22amtics009@gmail.com>
2025-06-27 18:25:09 +0530

    fix(UI):Tile icon bg

--- src/action/svgs/Svg.jsx
+++ src/action/svgs/Svg.jsx
@@ -1,6 +1,6 @@
 export const LocationIcon = ({ width = 24, height = 24 }) => (
   <svg width={width} height={height} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <rect width="24" height="24" fill="var(--background-glass)" />
+    <rect width="24" height="24" fill="none" />
     <path
       d="M12 21C15.5 17.4 19 14.1764 19 10.2C19 6.22355 15.866 3 12 3C8.13401 3 5 6.22355 5 10.2C5 14.1764 8.5 17.4 12 21Z"
       stroke={'var(--svg-color)'}
@@ -21,7 +21,7 @@ export const LocationIcon = ({ width = 24, height = 24 }) => (
 
 export const CalendarIcon = ({ width = 24, height = 24 }) => (
   <svg width={width} height={height} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <rect width="24" height="24" fill="var(--background-glass)" />
+    <rect width="24" height="24" fill="none" />
     <path
       d="M19,4H17V3a1,1,0,0,0-2,0V4H9V3A1,1,0,0,0,7,3V4H5A3,3,0,0,0,2,7V19a3,3,0,0,0,3,3H19a3,3,0,0,0,3-3V7A3,3,0,0,0,19,4Zm1,15a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V12H20Zm0-9H4V7A1,1,0,0,1,5,6H7V7A1,1,0,0,0,9,7V6h6V7a1,1,0,0,0,2,0V6h2a1,1,0,0,1,1,1Z"
       fill={'var(--svg-color)'}
